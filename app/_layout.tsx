4bf101cae424b3b58a75abe1ea6a1acfc73b2ade
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-16 21:35:12 +0600

    add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,32 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <ThemeProvider value={DarkTheme}>
+            <Stack>
+                <Stack.Screen name="+not-found" />
+            </Stack>
+        </ThemeProvider>
+    );
 }
